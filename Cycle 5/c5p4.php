eaad06f7ea7a03c6c606c12792a0c592c80b4969
subezio-whitehat <74660211+subezio-whitehat@users.noreply.github.com>
2021-03-01 15:16:22 +0530

    html cycle 5

---
+++ Cycle 5/c5p4.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+<body>
+<h1 align="center"> <u>Student Registration Form</u>
+<form action = "regdata.php" method = "POST">
+Name:
+<input type = "text" name = "txtname">
+<br><br>
+Roll no.:
+<input type = "text" name = "txtr_no">
+<br><br>
+Address:
+<textarea name = "add" type = "textarea"></textarea>
+<br><br>
+Contyact No:
+<input type = "text" name = "txtc_no">
+<br><br>
+Email ID:
+<input type = "text" name = "txteid">
+<br><br>
+<input type = "Submit" name = "insert" value = "Save">
+<input type = "Reset" value = "Cancel">
+</form>
+</body>
+</html>
